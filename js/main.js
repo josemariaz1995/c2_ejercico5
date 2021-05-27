36029b5f7ffa82fea16b25dcd403df658a5d0ba3
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 22:02:04 +0200

    arreglo de eslint

--- js/main.js
+++ js/main.js
@@ -2,16 +2,13 @@ const palindromo = "AÍbo,fobía";
 const noPalindromo = "yoJose";
 function esPalindromo(string) {
   let formateo = string;
-  let formateoReverse;
-  let formateoArray;
-  let formateoReverseArray;
   let esPalindoromo = false;
   formateo = formateo.toLowerCase();
   formateo = formateo.replace(/[.,:;()_?¿!¡-\s]/g, "");
   formateo = formateo.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  formateoArray = formateo.split("");
-  formateoReverseArray = formateoArray.reverse();
-  formateoReverse = formateoReverseArray.join("");
+  const formateoArray = formateo.split("");
+  const formateoReverseArray = formateoArray.reverse();
+  const formateoReverse = formateoReverseArray.join("");
   console.log(formateoReverse);
   console.log(formateo);
   if (formateo === formateoReverse) {
